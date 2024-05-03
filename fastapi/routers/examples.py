2286249b138a5cf1ef88b95342dfdce1c73fe9de
Adam Gerard <adam.intae.gerard@protonmail.com>
2024-05-03 15:51:37 -0500

    psycopg3, singleton conn

--- fastapi/routers/examples.py
+++ fastapi/routers/examples.py
@@ -1,16 +1,19 @@
 from fastapi import APIRouter
-import psycopg2
+import psycopg #psycopg3
 
 examples_api = APIRouter()
 
+# Singleton
+conn = psycopg.connect(dbname="postgres", user="testuser", password="testpassword", host="postgres", port="5432")
+
 @examples_api.get("/examples")
 async def examples():
-    conn = psycopg2.connect(dbname="postgres", user="testuser", password="testpassword", host="postgres", port="5432")
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM example;")
-    record_set = cur.fetchone()
+    # the following will automatically close the cursor after the block completes
+    # https://www.psycopg.org/psycopg3/docs/api/connections.html
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM example;")
+        record_set = cur.fetchone()
     
-    cur.close()
-    conn.close()
-    return record_set
+        #cur.close() # will auto close
+        #conn.close() # will be kept open
+        return record_set
--- serverless/handler.py
+++ serverless/handler.py
@@ -1,30 +1,31 @@
 import json
-import psycopg2
+import psycopg #psycopg3
 import urllib3
 
+# Singleton
+conn = psycopg.connect(dbname="postgres", user="testuser", password="testpassword", host="postgres", port="5432")
+
 def examples(event, context):
-    conn = psycopg2.connect(dbname="postgres", user="testuser", password="testpassword", host="postgres", port="5432")
+    # the following will automatically close the cursor after the block completes
+    # https://www.psycopg.org/psycopg3/docs/api/connections.html
+    with conn.cursor() as cur:
+        cur.execute("SELECT * FROM example;")
+        record_set = cur.fetchone()
 
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM example;")
-    record_set = cur.fetchone()
-
-    cur.close()
-    conn.close()
-
-    return {"statusCode": 200, "body": json.dumps(record_set)}
+        #cur.close() # will auto close
+        #conn.close() # will be kept open
+        return {"statusCode": 200, "body": json.dumps(record_set)}
 
 def psql_json(event, context):
-    conn = psycopg2.connect(dbname="postgres", user="testuser", password="testpassword", host="postgres", port="5432")
+    # the following will automatically close the cursor after the block completes
+    # https://www.psycopg.org/psycopg3/docs/api/connections.html
+    with conn.cursor() as cur:
+        cur.execute("SELECT arr -> 'id' AS json_id, arr -> 'name' AS json_name FROM example e, json_array_elements(e.json_array_col) arr WHERE (arr ->> 'id')::int > -1;")
+        record_set = cur.fetchone()
 
-    cur = conn.cursor()
-    cur.execute("SELECT arr -> 'id' AS json_id, arr -> 'name' AS json_name FROM example e, json_array_elements(e.json_array_col) arr WHERE (arr ->> 'id')::int > -1;")
-    record_set = cur.fetchone()
-
-    cur.close()
-    conn.close()
-
-    return {"statusCode": 200, "body": json.dumps(record_set)}
+        #cur.close() # will auto close
+        #conn.close() # will be kept open
+        return {"statusCode": 200, "body": json.dumps(record_set)}
 
 def other_api(event, context):
     http = urllib3.PoolManager()
